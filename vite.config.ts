281af9c34bc065274ea4e90eadabb8e9e36e232a
SebaRamirez <sebaraf400@gmail.com>
2026-07-16 22:20:40 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/gestorGastos106/' : '/',
+    base: process.env.NODE_ENV === 'production' ? '/web-gestor-106/' : '/',
     plugins: [plugin()],
     resolve: {
         alias: {
